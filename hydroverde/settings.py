c3d04263e47c9554ed1ef29537d1663f2376c618
gabbygood <gabriellegoylan@gmail.com>
2025-07-02 22:34:46 +0800

    update

--- hydroverde/settings.py
+++ hydroverde/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h=zh^-g*urmp=8%^&&1e7!rlbyd=y4raw+(sw66g)&0ohmc*+w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['tempdata.onrender.com']
 
