710ad10efa706a832af5b75e54a12396153b8a97
gabbygood <gabriellegoylan@gmail.com>
2025-07-03 02:38:10 +0800

    update

--- sensors/views.py
+++ sensors/views.py
@@ -1,13 +1,20 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import SensorData
+from .serializers import SensorDataSerializer  # You'll create this next
 
-@api_view(['POST'])
+@api_view(['GET', 'POST'])
 def receive_data(request):
-    temp = request.data.get('temperature')
-    hum = request.data.get('humidity')
+    if request.method == 'POST':
+        temp = request.data.get('temperature')
+        hum = request.data.get('humidity')
 
-    if temp is not None and hum is not None:
-        SensorData.objects.create(temperature=temp, humidity=hum)
-        return Response({"status": "Data saved"}, status=201)
-    return Response({"error": "Invalid data"}, status=400)
+        if temp is not None and hum is not None:
+            SensorData.objects.create(temperature=temp, humidity=hum)
+            return Response({"status": "Data saved"}, status=201)
+        return Response({"error": "Invalid data"}, status=400)
+
+    if request.method == 'GET':
+        data = SensorData.objects.order_by('-timestamp')[:50]  # latest 50 records
+        serializer = SensorDataSerializer(data, many=True)
+        return Response(serializer.data)
